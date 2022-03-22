681ef19d58187f0f379ba67f5c47498291ab0b59
Chris Keller <xylo04@gmail.com>
2022-03-22 17:34:14 -0600

    Fix detection of Packages
    Apparently this is not a boolean, so no square brackets

--- 02-make-macos.sh
+++ 02-make-macos.sh
@@ -2,18 +2,20 @@
 set -e
 PAT_ROOT="../pat"
 VERSION=$(grep "Version =" ${PAT_ROOT}/internal/buildinfo/VERSION.go|cut -d '"' -f2)
+ARCH=$(go env GOARCH)
 
 if [ $(go env GOOS) != "darwin" ]; then
 	echo "This step must be run on macOS" && exit 1
 fi
-if [ ! $(command -v packagesbuild >/dev/null 2>&1) ]; then
+if ! $(command -v packagesbuild >/dev/null 2>&1) ; then
 	echo "WhiteBox Packages must be installed" && exit 1
 fi
 (cd ${PAT_ROOT}; ./make.bash;)
 
 if [ -d $SIGNING_KEY ]; then
 	echo "WARNING: No SIGNING_KEY set. Using unsigned pkg for distribution."
-	cp ${PAT_ROOT}/pat_${VERSION}_darwin_amd64_unsigned.pkg dists/pat_${VERSION}_darwin_amd64.pkg
+	mkdir -p dists
+	cp ${PAT_ROOT}/pat_${VERSION}_darwin_${ARCH}_unsigned.pkg dists/pat_${VERSION}_darwin_${ARCH}.pkg
 else
-	productsign --sign "${SIGNING_KEY}" ${PAT_ROOT}/pat_${VERSION}_darwin_amd64_unsigned.pkg dists/pat_${VERSION}_darwin_amd64.pkg
+	productsign --sign "${SIGNING_KEY}" ${PAT_ROOT}/pat_${VERSION}_darwin_${ARCH}_unsigned.pkg dists/pat_${VERSION}_darwin_${ARCH}.pkg
 fi
